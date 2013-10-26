49763cf61b0eb7d275ce911bd17a710786ad8f05
Andriy Borodiychuk <a.borodiychuk@markusweb.com>
2013-10-26 23:06:28 +0200

    Bugfix

--- background.js
+++ background.js
@@ -3,10 +3,3 @@ chrome.extension.onRequest.addListener(function (request, sender, sendResponse) 
   sendResponse({});
 });
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if(changeInfo.status !== 'complete') return;
-  if(tab.url.indexOf('www.ebay') === -1) return;
-  chrome.tabs.executeScript(null, {file: 'jquery.js'});
-  chrome.tabs.executeScript(null, {file: 'core.js'});
-});
-
